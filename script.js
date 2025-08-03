afeccb9d509e17d72c293160361abedd494afd35
Verl777 <morahvalerianne@gmail.com>
2025-08-03 19:55:38 +0300

    To do list

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ document.addEventListener('DOMContentLoaded', () => {
         // Create a remove button
         const removeButton = document.createElement('button');
         removeButton.textContent = "Remove";
-        removeButton.className = "remove-btn";
+        removeButton.classList.add('remove-btn');
 
         // Remove task when remove button is clicked
         removeButton.onclick = () => {
